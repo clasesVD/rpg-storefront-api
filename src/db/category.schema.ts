f09960d86cde3556da225bddd69191b68dd363a8
Bruno Villavicencio <bruno.villavicencio@xcons.com>
2025-01-28 21:02:07 -0300

    feat(RPGSF-20): fix db schema name unique

--- src/db/category.schema.ts
+++ src/db/category.schema.ts
@@ -2,7 +2,7 @@ import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 const categoriesTable = pgTable("categories", {
   id: uuid().defaultRandom().primaryKey(),
-  name: varchar({ length: 255 }).notNull(),
+  name: varchar({ length: 255 }).unique().notNull(),
 });
 
 export default categoriesTable;
