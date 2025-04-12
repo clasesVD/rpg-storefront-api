f6d0718c741da617aa923724d6cb44ee39a58253
Exequiel Segura <exequiel.segura@hotmail.com>
2025-04-12 17:43:17 -0300

    fix(RPGSF-57): remove comment and rebase.

--- drizzle/0004_bumpy_shinobi_shaw.sql
+++ drizzle/0004_bumpy_shinobi_shaw.sql
@@ -1,2 +1,2 @@
-CREATE TYPE "public"."user_role" AS ENUM('A', 'C');--> statement-breakpoint
+CREATE TYPE "public"."user_role" AS ENUM('A', 'C');
 ALTER TABLE "user" ADD COLUMN "role" "user_role" NOT NULL;
